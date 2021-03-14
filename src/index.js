08a879e4d7c3c5ae131a33c32d1ffb0994c0eb6b
PavelM1988 <79479113+PavelM1988@users.noreply.github.com>
2021-03-14 15:15:20 +0300

    init: push to github index.js file from local rep

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
-
 // You should implement your task here.
-
 module.exports = function towelSort (matrix) {
-  return [];
+  if (!matrix || matrix.length === 0){return [];}
+    let res = [];
+       for(let i =0; i < matrix.length; i++){
+    if (i % 2 == 1){
+       for(let j = matrix[i].length - 1; j >= 0; j--){res.push(matrix[i][j]);}
+    }
+    else{
+        for(let j = 0; j < matrix[i].length; j++){res.push(matrix[i][j]);}
+    }
+  }
+return res;
 }
